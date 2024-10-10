fe20d895f1c4d1a55fc54e34a56b7902dcd60981
ikermtz <49227870+ikermtz@users.noreply.github.com>
2024-10-10 19:32:25 +0200

    Update control_robot.py

--- control_robot.py
+++ control_robot.py
@@ -1,3 +1,4 @@
+# Imports
 import sys
 import copy
 import rospy
@@ -10,7 +11,10 @@ from moveit_commander.conversions import pose_to_list
 from typing import List
 from geometry_msgs.msg import Pose, PoseStamped
 
+# Clase para controlar las funciones del robot
 class ControlRobot:
+
+    # Constructor de la clase para controlar las funciones del robot
     def __init__(self) -> None:
         moveit_commander.roscpp_initialize(sys.argv)
         rospy.init_node("control robot", anonymous=True)
@@ -20,19 +24,24 @@ class ControlRobot:
         self.move_group = moveit_commander.MoveGroupCommander(self.group_name)
         self.add_floor()
 
+    # Devolvemos una lista de los angulos de los motores
     def get_motor_angles(self) -> list:
         return self.move_group.get_current_joint_values()
     
+    # Movemos los motores del robot a la posicion que le indicamos
     def move_motors(self, joint_goal: List[float], wait: bool=True) -> bool:
         return self.move_group.go(joint_goal, wait=wait)
 
+    # Obtenemos la pose del efector final del robot
     def get_pose(self) -> Pose:
         return self.move_group.get_current_pose().pose
 
+    # Mover el efector final del robot a una pose indicada, devuelve True al finalizar
     def move_to_pose(self, pose_goal: Pose, wait: bool=True) -> bool:
         self.move_group.set_pose_target(pose_goal)
         return self.move_group.go(wait=wait)
 
+    # Añadimos un obstaculo, en este caso una caja
     def add_box_to_planning_scene(self, pose_caja: Pose, name: str, tamaño: tuple = (.1, .1, .1)) -> None:
         box_pose = PoseStamped()
         box_pose.header.frame_id = "world"
@@ -40,25 +49,21 @@ class ControlRobot:
         box_name = name
         self.scene.add_box(box_name, box_pose, size=tamaño)
 
+    # Añadimos el suelo
     def add_floor(self) -> None:
         pose_suelo = Pose()
         pose_suelo.position.z = -0.025
         self.add_box_to_planning_scene(pose_suelo, "suelo", (2,2,.05))
 
-    def move_to_path(path) -> None:
-        points = []     
+    # Indicamos una trayectoria al robot, devuelve True al finalizar
+    def move_trajectory(self, poses: List[Pose], wait: bool = True) -> bool:
+        (plan, fraction) = self.move_group.compute_cartesian_path(poses, 0.01, 0.0)
 
-        wpose = control.get_pose()
-        wpose.position.z -= wpose.position.z * 0.1
-        wpose.position.y -= wpose.position.y * 0.1
-        wpose.position.x -= wpose.position.x * 0.1
-        points.append(copy.deepcopy(wpose))
+        if fraction != 1.0:
+            return False
+        
+        return self.move_group.execute(plan, wait = wait)
 
-        wpose2 = control.get_pose()
-        wpose2.position.z -= wpose2.position.z * 0.2
-        wpose2.position.y -= wpose2.position.y * 0.2
-        wpose2.position.x -= wpose2.position.x * 0.2
-        points.append(copy.deepcopy(wpose2))
 
 if __name__ == '__main__':
     control = ControlRobot()
